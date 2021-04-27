4c38ef9018a213184df005fc1614f86ee21664b8
reynandajm <reynandajuliansyahm@gmail.com>
2021-04-27 10:45:00 +0700

    Rev Product

--- src/components/ItemsQiow.vue
+++ src/components/ItemsQiow.vue
@@ -46,6 +46,7 @@
         <!-- First Row [Prosucts]-->
     
     <div class="row pb-5 mb-4">
+        <!-- Bola susu coklat -->
         <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
             <!-- Card-->
             <div class="card rounded shadow-sm border-0">
@@ -84,7 +85,7 @@
                 </div>
             </div>
         </div>
-        
+        <!-- Nastar -->
         <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
             <!-- Card-->
             <div class="card rounded shadow-sm border-0">
@@ -122,7 +123,7 @@
                 </div>
             </div>
         </div>
-        
+        <!-- Coklat Kacang -->
         <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
             <!-- Card-->
             <div class="card rounded shadow-sm border-0">
@@ -160,7 +161,7 @@
                 </div>
             </div>
         </div>
-        
+        <!-- Keju -->
         <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
             <!-- Card-->
             <div class="card rounded shadow-sm border-0">
@@ -200,7 +201,164 @@
             </div>
         </div>
     </div>
-        
+    <div class="row pb-5 mb-4">
+        <!-- Kacang Putih -->
+        <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                <!-- Card-->
+                <div class="card rounded shadow-sm border-0">
+                    <div class="card-body p-4">
+                        <div class="product-item">
+                        <div class="pi-pic">
+                        <img src="img/cookies/kacang_putih.jpg" alt="" class="img-fluid d-block mx-auto mb-3">
+                                    <ul>
+                                        <li class="w-icon active">
+                                            <a href="#"><i class="icon_bag_alt"></i></a>
+                                        </li>
+                                        <li class="quick-view"><a href="#">+ Quick View</a></li>
+                                    </ul>
+                        </div>
+                        </div>
+                        <div class="product-item">
+                        <div class="pi-text">
+                                    <div class="catagory-name">Kue Kering</div>
+                                    <a href="/#/product">
+                                        <h5>Kue Keju</h5>
+                                    </a>
+                                    <div class="product-price">
+                                        Rp.60.000
+                                        <span>Rp.65.000</span>
+                                    </div>
+                        </div>
+                        </div>
+                        <ul class="list-inline small">
+                            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                            
+                        </ul>
+                    </div>
+                </div>
+        </div>
+        <!-- Bintang -->
+        <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                <!-- Card-->
+                <div class="card rounded shadow-sm border-0">
+                    <div class="card-body p-4">
+                        <div class="product-item">
+                        <div class="pi-pic">
+                        <img src="img/cookies/bintang.jpg" alt="" class="img-fluid d-block mx-auto mb-3">
+                                    <ul>
+                                        <li class="w-icon active">
+                                            <a href="#"><i class="icon_bag_alt"></i></a>
+                                        </li>
+                                        <li class="quick-view"><a href="#">+ Quick View</a></li>
+                                    </ul>
+                        </div>
+                        </div>
+                        <div class="product-item">
+                        <div class="pi-text">
+                                    <div class="catagory-name">Kue Kering</div>
+                                    <a href="/#/product">
+                                        <h5>Kue Keju</h5>
+                                    </a>
+                                    <div class="product-price">
+                                        Rp.55.000
+                                        <span>Rp.60.000</span>
+                                    </div>
+                        </div>
+                        </div>
+                        <ul class="list-inline small">
+                            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                            
+                        </ul>
+                    </div>
+                </div>
+        </div>
+        <!-- panjang -->
+        <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                <!-- Card-->
+                <div class="card rounded shadow-sm border-0">
+                    <div class="card-body p-4">
+                        <div class="product-item">
+                        <div class="pi-pic">
+                        <img src="img/cookies/panjang.jpg" alt="" class="img-fluid d-block mx-auto mb-3">
+                                    <ul>
+                                        <li class="w-icon active">
+                                            <a href="#"><i class="icon_bag_alt"></i></a>
+                                        </li>
+                                        <li class="quick-view"><a href="#">+ Quick View</a></li>
+                                    </ul>
+                        </div>
+                        </div>
+                        <div class="product-item">
+                        <div class="pi-text">
+                                    <div class="catagory-name">Kue Kering</div>
+                                    <a href="/#/product">
+                                        <h5>Kue Keju</h5>
+                                    </a>
+                                    <div class="product-price">
+                                        Rp.45.000
+                                        <span>Rp.60.000</span>
+                                    </div>
+                        </div>
+                        </div>
+                        <ul class="list-inline small">
+                            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                            
+                        </ul>
+                    </div>
+                </div>
+        </div>
+        <!-- semprit -->
+        <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                <!-- Card-->
+                <div class="card rounded shadow-sm border-0">
+                    <div class="card-body p-4">
+                        <div class="product-item">
+                        <div class="pi-pic">
+                        <img src="img/cookies/semprit.jpg" alt="" class="img-fluid d-block mx-auto mb-3">
+                                    <ul>
+                                        <li class="w-icon active">
+                                            <a href="#"><i class="icon_bag_alt"></i></a>
+                                        </li>
+                                        <li class="quick-view"><a href="#">+ Quick View</a></li>
+                                    </ul>
+                        </div>
+                        </div>
+                        <div class="product-item">
+                        <div class="pi-text">
+                                    <div class="catagory-name">Kue Kering</div>
+                                    <a href="/#/product">
+                                        <h5>Kue Keju</h5>
+                                    </a>
+                                    <div class="product-price">
+                                        Rp.50.000
+                                        <span>Rp.65.000</span>
+                                    </div>
+                        </div>
+                        </div>
+                        <ul class="list-inline small">
+                            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                            <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+                            
+                        </ul>
+                    </div>
+                </div>
+        </div>   
+    </div>
     </section>
     <!-- Women Banner Section End -->
 </template>
